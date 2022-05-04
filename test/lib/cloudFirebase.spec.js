f137924100fb077b14379fb0a539b437ad3a13cf
Tenshi <64874044+Myriam555@users.noreply.github.com>
2022-05-04 09:57:32 -0500

    merge cloudFirebase.spec.js

--- test/lib/cloudFirebase.spec.js
+++ test/lib/cloudFirebase.spec.js
@@ -1,5 +1,4 @@
 import { dataUser, likePublication, lovePublication } from '../../src/lib/cloudFirebase';
-import { setDoc } from '../../src/lib/imports/firebase-imports';
 
 jest.mock('../../src/lib/imports/firebase-imports.js');
 
@@ -8,15 +7,14 @@ describe('cloudfirebase', () => {
     const data = await dataUser('id', 'email', 'password', 'url', 'url');
     expect(data).toBe('id');
   });
-  it('dataUser error', async () => {
-    const data = await dataUser();
-    expect(data).toBe('id');
-  });
   it('likePublication', async () => {
+    sessionStorage.setItem('uid', 'Umn8appNPisPz4eBhswX');
     const like = await likePublication('Umn8appNPisPz4eBhswX');
     expect(like).toBe(true);
   });
-  it('lovePublication', () => {
-    expect(true).toBe(true);
+  it('lovePublication', async () => {
+    sessionStorage.setItem('uid', 'Umn8appNPisPz4eBhswX');
+    const love = await lovePublication('Umn8appNPisPz4eBhswX');
+    expect(love).toBe(true);
   });
 });
